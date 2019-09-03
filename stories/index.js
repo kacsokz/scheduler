6c1d701b557c717a878ac5b67da99a218ce0d00c
kacsokz <caseysokach@gmail.com>
2019-09-03 16:37:14 -0600

    Added more views for Application component

--- stories/index.js
+++ stories/index.js
@@ -11,8 +11,12 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
-import Header from "components/Appointment/Header"
-import Empty from "components/Appointment/Empty"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // Stories of Button
 storiesOf("Button", module)
@@ -146,4 +150,10 @@ storiesOf("InterviewerList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" onEdit={action("onEdit")} onDelete={action("onDelete")} />)
+  .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Saving", () => <Status message="Saving" />)
+  .add("Deleting", () => <Status message="Deleting" />)
+  .add("Error Saving", () => <Error message="Could not save appointment." onClose={action("onClose")} />)
+  .add("Error Deleting", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
