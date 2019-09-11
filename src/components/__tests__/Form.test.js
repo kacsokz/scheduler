cf63d5275ae6a14ba98471d9b7d8e9bf94c3e741
kacsokz <caseysokach@gmail.com>
2019-09-11 15:32:16 -0600

    Linting

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -40,7 +40,6 @@ describe("Form", () => {
       />
       );
     fireEvent.click(getByText("Save"));
-
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
@@ -50,20 +49,14 @@ describe("Form", () => {
     const { getByText, getByPlaceholderText, queryByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
-  
     fireEvent.click(getByText("Save"));
-  
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
-  
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
       target: { value: "Lydia Miller-Jones" }
     });
-  
     fireEvent.click(getByText("Save"));
-  
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
@@ -78,19 +71,13 @@ describe("Form", () => {
         onCancel={onCancel}
       />
     );
-  
     fireEvent.click(getByText("Save"));
-  
     fireEvent.change(getByPlaceholderText("Enter Student Name"), {
       target: { value: "Lydia Miller-Jones" }
     });
-  
     fireEvent.click(getByText("Cancel"));
-  
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
-  
     expect(onCancel).toHaveBeenCalledTimes(1);
   });
 
